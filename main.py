ab9553014479bbc72280ca1f11d69f6bd717103e
Jonathan Riezman <riezman@gmail.com>
2017-10-10 21:56:23 -0500

    added User class and modified Blog to have ForeignKey(user.id)

--- main.py
+++ main.py
@@ -15,11 +15,24 @@ class Blog(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(50))
 	body = db.Column(db.Text)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 	
-	def __init__(self, title, body):
+	def __init__(self, title, body, user):
 		self.title = title
 		self.body = body
+		self.user = user
 		
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(24), unique=True)
+	password = db.Column(db.String(24))
+	blogs = db.relationship('Blog', backref='user')
+	
+	def __init__(self,username,password):
+		self.username = username
+		self.password = password
+
+# Flask handler routes
 @app.route("/blog")
 def blog():
 	# if there is an id argument render just that blog
