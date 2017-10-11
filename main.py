34b9da2e5f15270910aeeb7c7ef6ca6f7c55ef05
Jonathan Riezman <riezman@gmail.com>
2017-10-10 21:12:30 -0500

    changed SQLALCHEMY_DATABASE_URI to blogz instead of build-a-blog

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sort import reverse_bubble_sort
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:catfish@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:catfish@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = "catfish"
 
